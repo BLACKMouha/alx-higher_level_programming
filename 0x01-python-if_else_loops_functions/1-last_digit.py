31567c58c82faa60753860db7e9c2a1b7476c776
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-26 08:54:46 +0000

    The Last digit - Rise of the Python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ number = str(number)
 len_number = len(number)
 last_digit = int(number[len_number - 1])
 
-print('Last digit', number, 'is', last_digit, end='')
+print('Last digit of', number, 'is', last_digit, end='')
 if last_digit > 5:
     print(' and is greater than 5')
 if last_digit < 6 and last_digit != 0:
