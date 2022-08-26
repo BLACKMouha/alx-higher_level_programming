c8e9deef74f80ae52207d5113234d3dce2f64b72
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-26 09:49:13 +0000

    Lowercase Alphabet with ASCII code

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
-"""
-Write a function that prints the ASCII alphabet, not
-followed by a new line
-
-- You can only use 1 print() funtion with string format
-- You can only use one loop in your code
-- You are not allowed to store characters in a variable
-- You are not allowed to import any module
-"""
-for ascii_code in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(ascii_code), end='')
+for ascii_code in range(97, 123):
+    print("{letter}".format(letter=chr(ascii_code)), end='')
