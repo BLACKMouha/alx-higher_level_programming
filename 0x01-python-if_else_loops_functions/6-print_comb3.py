7346adaa65a4eaf55f3d35ce8c129c96b7c996f6
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-27 13:52:51 +0000

    Printing conditional combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for first in range(10):
     for last in range(10):
         if first < last:
-            if first != 8 and last != 9:
-                print('{}{}, '.format(first, last), end='')
             if first == 8 and last == 9:
-                print('{}{}'.format(first, last))
+                print('{}{}, '.format(first, last))
+            else:
+                print('{}{}, '.format(first, last), end='')
