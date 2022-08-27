fb81a42dcec06a3e55d364c226fc57642b86db1a
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-27 16:14:44 +0000

    Python function that prints a string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,47 @@
 #!/usr/bin/python3
+"""
 
+Write a function that prints a string in uppercase
+followed by a new line
+
+Prototype: def uppercase(str):
+You can only use one loop in your code
+You are not allowed to import any module
+You are not allowed to use str.upper() and str.isupper()
+
+"""
+
+
+"""
+
+To print a string in uppercase, each character that is
+lowercase is converted and printed in uppercase. Others
+characters are printed as-is.
+
+"""
+lowercase = __import__('7-islower').lowercase
 def uppercase(str):
     for c in str:
-        if 64 < ord(c) < 91 or c == ' ':
-            print(c, end='')
-        else:
-            print(chr(ord(c) - 32), end='')
+    	print('{}'.format(c if lowercase(c) else chr(ord(c) - 32)), end='')
     print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
