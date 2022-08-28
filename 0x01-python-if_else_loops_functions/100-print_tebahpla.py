1edb2971aed241c77239d42b681efe3e8fa45308
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-28 19:10:31 +0000

    Printing alphabet in a lowercaseUPPERCASE format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for c in range(65, 91):
-    print('{}'.format(chr(c + 32) if c % 2 == 0 else chr(c)), end='')
+for c in range(122, 96, -1):
+    print('{}'.format(chr(c) if c % 2 == 0 else chr(c - 32)), end='')
