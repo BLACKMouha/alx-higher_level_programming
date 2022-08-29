7b68b643f02dbcb94c0158070481637002c67452
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-29 22:24:12 +0000

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,12 +4,12 @@
 if __name__ == "__main__":
     from sys import argv
     len_argv = len(argv)
-    if len_argv > 1:
-        len_argv = 0
-        result = 0
-        for arg in argv:
-            if len_argv != 0:
-                arg = int(arg)
-                result += arg
-            len_argv += 1
-        print(result)
+
+    len_argv = 0
+    result = 0
+    for arg in argv:
+        if len_argv != 0:
+            arg = int(arg)
+            result += arg
+        len_argv += 1
+    print(result)
