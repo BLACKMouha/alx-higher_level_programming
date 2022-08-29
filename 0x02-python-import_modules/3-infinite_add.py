c2434b6f701b2e7201babf74c01675160eef7828
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-29 22:20:24 +0000

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    from sys import argv
+    len_argv = len(argv) - 1
+    if len_argv >= 1:
+        len_argv = 0
+        result = 0
+        for arg in argv:
+            if len_argv != 0:
+                arg = int(arg)
+                result += arg
+            len_argv += 1
+        print(result)
