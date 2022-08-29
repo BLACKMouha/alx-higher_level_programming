bcdbe0459ff8295dbe0518faa9de96109151da1b
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-29 22:07:07 +0000

    Arguments passed to program with Python3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from sys import argv
-    len_argv = len(argv) - 1
+    import sys
+    len_argv = len(sys.argv) - 1
 
     if len_argv == 0:
-        print('{} arguments.:'.format(len_argv))
+        print("{} arguments.".format(len_argv))
     elif len_argv == 1:
-        print('{} argument:'.format(len_argv))
+        print("{} argument:".format(len_argv))
     else:
-        print('{} arguments:'.format(len_argv))
+        print("{} arguments:".format(len_argv))
 
-    if len_argv > 0:
+    if len_argv >= 1:
         i = 0
-        for arg in argv:
-            if i > 0:
-                print('{}: {}'.format(i, arg))
+        for arg in sys.argv:
+            if len_argv != 0:
+                print("{}: {}".format(i, arg))
             i += 1
