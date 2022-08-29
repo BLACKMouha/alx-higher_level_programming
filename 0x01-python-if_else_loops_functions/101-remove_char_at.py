05d492a3cb56b1b03d9f031bde6459ade1a31f71
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-29 10:55:07 +0000

    Remove character from a string at a given index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    if n < len(str):
-        for i in range(len(str)):
-            if i != n:
-                print(str[i], end='')
+    if 0 < n < (len(str) - 1):
+        return str
+
+    final_str = ""
+    for i in range(0, len(str), 1):
+        if i == n:
+            continue
+        else:
+            final_str += str[i]
+    return str
