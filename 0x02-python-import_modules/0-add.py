b8a9499f30298b4093682cc3dadb3055ec5fbfb0
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-29 16:30:24 +0000

    Import a custom function for sum

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-import add_0
+from add_0 import add
 
 if __name__ == "__main__":
-    a, b = 1, 2
-    print('{0} + {1} = {2}'.format(a, b, add_0.add(1, 2)))
+    a =1
+    b = 2
+    print('{0} + {1} = {2}'.format(a, b, add(1, 2)))
