49ad512ecba97204a12546a233a8a0347f9d2b95
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-29 16:41:37 +0000

    Import a custom function for sum

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print('{n1} + {n2} = {r} '.format(r=add(a, b), n1=a, n2=b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
