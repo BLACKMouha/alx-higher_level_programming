5996f1b47885745bc196208a9786f3c25b629868
BLACKMouha <mohamedsenghor05gmail.com>
2022-08-30 11:54:11 +0000

    Basic Maths operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     elif op == '/':
         print('{} {} {} = {:.0f}'.format(a, op, b, div(a, b)))
     else:
-        print("Unknown operator. Avaiblable operators +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
