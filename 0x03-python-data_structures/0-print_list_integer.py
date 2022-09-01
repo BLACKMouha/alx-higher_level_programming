1dcdaa828e0d27bf4f651bec9aea3ea7b0757354
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-01 12:04:24 +0000

    Function that print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,7 +3,3 @@
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print('{0:d}'.format(my_list[i]))
-
-if __name__ == "__main__":
-    list_int = [1, 2, 3, 4]
-    print_list_integer(list_int)
