bd96ba24f1397664c92953b5141e4b14b4fa137f
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-01 16:02:27 +0000

    Printing a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+    for sublist in matrix:
+        for idx in range(len(sublist)):
+            if idx == len(sublist) - 1:
+                print("{}".format(sublist[idx]))
+            if idx != len(sublist) - 1:
+                print("{} ".format(sublist[idx]), end='')
