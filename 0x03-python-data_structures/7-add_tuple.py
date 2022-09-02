2898514e035325ef9f75ee6011869fdfe71d387d
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-02 13:34:08 +0000

    Function returns a tuple of the sum of items at the same index of two distincts tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_a) == 2:
-        t_a_1 = tuple_a[0]
-        t_a_2 = tuple_a[1]
+    if len(tuple_a) == 0:
+        a1 = 0
+        a2 = 0
     elif len(tuple_a) == 1:
-        t_a_1 = tuple_a[0]
-        t_a_2 = 0
+        a1 = tuple_a[0]
+        a2 = 0
     else:
-        t_a_1 = 0
-        t_a_2 = 0
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
 
-    if len(tuple_b) == 2:
-        t_b_1 = tuple_b[0]
-        t_b_2 = tuple_b[1]
+    if len(tuple_b) == 0:
+        b1 = 0
+        b2 = 0
     elif len(tuple_b) == 1:
-        t_b_1 = tuple_b[0]
-        t_b_2 = 0
+        b1 = tuple_b[0]
+        b2 = 0
     else:
-        t_b_1 = 0
-        t_b_2 = 0
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
 
-    sum_tuple = (t_a_1 + t_b_1, t_a_2 + t_b_2)
+    new_tuple = (a1 + b1, a2 + b2)
 
-    return sum_tuple
+    return (new_tuple)
