4c7431642feda7643f7c327cc900f6be254ad8d8
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-02 13:02:47 +0000

    Function returns a tuple of the sum of items at the same index of two distincts tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,33 +2,26 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     sum_tuple = ()
 
-    if len(tuple_a) > len(tuple_b):
-        idx = 0
-        for i in range(len(tuple_b)):
-            s = tuple_a[i] + tuple_b[i]
-            sum_tuple += s,
-            idx += 1
-        for j in range(idx, len(tuple_a)):
-            sum_tuple += tuple_a[j],
+    if len(tuple_a) == 2:
+        t_a_1 = tuple_a[0]
+        t_a_2 = tuple_a[1]
+    elif len(tuple_a) == 1:
+        t_a_1 = tuple_a[0]
+        t_a_2 = 0
+    else:
+        t_a_1 = 0
+        t_a_2 = 0
 
-    if len(tuple_b) > len(tuple_a):
-        idx = 0
-        for i in range(len(tuple_a)):
-            s = tuple_a[i] + tuple_b[i]
-            sum_tuple += s,
-            idx += 1
-        for j in range(idx, len(tuple_b)):
-            sum_tuple += tuple_b[j],
+    if len(tuple_b) == 2:
+        t_b_1 = tuple_b[0]
+        t_b_2 = tuple_b[1]
+    elif len(tuple_b) == 1:
+        t_b_1 = tuple_b[0]
+        t_b_2 = 0
+    else:
+        t_b_1 = 0
+        t_b_2 = 0
 
-    if len(tuple_a) == len(tuple_b):
-        for i in range(len(tuple_b)):
-            s = tuple_a[i] + tuple_b[i]
-            sum_tuple += s,
+    sum_tuple = (t_a_1 + t_b_1, t_a_2 + t_b_2)
 
-    if not tuple_a and not tuple_b:
-        sum_tuple = 0, 0,
-    if tuple_a == () and not tuple_b:
-        sum_tuple = tuple_a
-    if not tuple_a == () and tuple_b:
-        sum_tuple = tuple_b
     return sum_tuple
