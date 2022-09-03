1a700e907e2d2d6eb0db9549c404443c184713cb
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-03 09:57:00 +0000

    Functions that finds the max value of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if my_list ==[]:
+        return None
     copy_my_list = my_list.copy()
     copy_my_list.sort()
+
     return copy_my_list[len(copy_my_list) - 1]
