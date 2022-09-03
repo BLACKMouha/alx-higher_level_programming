e285e26316281d0fa389b501e10da6920d056bcc
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-03 12:10:21 +0000

    Integer(s) divisible by 2 from a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,10 +3,10 @@ def divisible_by_2(my_list=[]):
     if my_list == []:
         return True
 
-    new_list = my_list.copy()
+    new_list = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            new_list[i] = True
+            new_list.append(True)
         else:
-            new_list[i] = False
+            new_list.append(False)
     return new_list
