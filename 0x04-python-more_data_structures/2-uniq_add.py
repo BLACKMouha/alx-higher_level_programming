8f9759f326f953f7e0fada6ae47d832e62b9c978
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-12 18:15:11 +0000

    Addition of unique item from a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    r = 0
+    my_set = set()
+
+    for i in my_list:
+        my_set.add(i)
+
+    del i
+
+    for i in my_set:
+        r += i
+    del my_set
+    return r
