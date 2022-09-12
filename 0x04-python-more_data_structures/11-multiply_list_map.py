315428ba3c0c75aebc98772358e50b8710779266
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-12 23:06:06 +0000

    Using map to create a new list

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if my_list is None or number is None:
-        return None
-    else:
-        return list(map(lambda x: x*number, list(my_list)))
+    return list(map(lambda x: x*number, list(my_list)))
