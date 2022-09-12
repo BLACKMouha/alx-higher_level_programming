3f79965b1e567505567e319cbf47b96917a8b9c9
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-12 16:32:50 +0000

    Square of a matrix | pycodestyle refactoring

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-
-"""
-Write a function that computes the square value of all integers of a matrix
-
-Prototype: def square_matrix_simple(matrix[]):
-
-Returns a new matrix
-	Same size as @matrix
-	Each value should be the square of the value of the input
-
-Initial matrix should not be modified
-
-You are not allowd to import any module
-
-"""
-
 def square_matrix_simple(matrix=[]):
-        return [[i**2 for i in row] for row in matrix]
+    return [[i**2 for i in row] for row in matrix]
