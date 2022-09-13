9b4305a4330ec891cc5d2e4d727ef78d0636dca8
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-13 21:43:27 +0000

    weighted average of all integers tuple (<score>, <weight>)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    sum_weights = 0
+    sum_score_weighted = 0
+    score_weighted = 1
+    average_weighted = 0
+
+    for item in my_list:
+        sum_weights += item[1]
+
+    for item in my_list:
+        score_weighted = 1
+        for i in range(len(item)):
+            score_weighted *= item[i]
+        sum_score_weighted += score_weighted
+    average_weighted = sum_score_weighted / sum_weights
+
+    return average_weighted
