21a00cac15ca88b20386c7a054501a7df0169c5a
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-22 12:28:06 +0000

    Defining the public instance method -area- of the class Square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-class Square:
-    """Definionn of a class Square."""
+"""Square class for representation of a real word square"""
 
-    def __init__(self, size):
-        """__init__(self, size)
-            Instantiates a square object with a given size
-            The size must be a integer greater than 0
-            Otherwise TypeError if size is not an integer or
-            ValueError is raised if size if less than 0.
+
+class Square:
+    """Define the class Square."""
+
+    def __init__(self, size=0):
+        """Called automaticcaly and immediately when instantiating a square
+            object with a given size
+
+            Args:
+                size (int): size of the square.
+
+            Raises:
+                TypeError: size if not type int.
+                ValueError: if size < 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -16,7 +23,9 @@ class Square:
         self.__size = size
 
     def area(self):
-        """area(self)
-            Return an integer representing the area for a
-            given square' size"""
+        """Computes the area of a square thanks to its size
+
+            Return:
+                int: an integer representing the area for a given square's
+                size"""
         return self.__size ** 2
