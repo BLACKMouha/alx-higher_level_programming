8586901473a7f328ff81b6c32be147ea63215aa3
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-22 15:59:27 +0000

    X-axix padded printing square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,8 +25,8 @@ class Square:
         self.__size = size
 
         if (not isinstance(position, tuple)
-                or not isinstance(position[0], int)
-                or not isinstance(position[1], int)
+                or (not isinstance(position[0], int)
+                and not isinstance(position[1], int))
                 or len(position) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
@@ -65,10 +65,9 @@ class Square:
 
             Return:
                 size of the square"""
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)
+        if (not isinstance(position, tuple)
+                or (not isinstance(position[0], int)
+                and not isinstance(position[1], int))
                 or len(position) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.position = value
