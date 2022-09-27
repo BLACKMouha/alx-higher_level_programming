be1d3f11794200aa4a6914f81c6072503d2baf5e
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-27 17:37:35 +0000

    Printing Firstname and Lastname | Docstring

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-
 """3-say_my_name.py"""
 
 
-"""Task: write a function that print 'My name is <first name> <last name>'.
-    Prototype: def say_name(first_name, last_name=""):
-
-    Args:
-        first_name (str): string
-        last_name (str): string. Default value is "" (empty string)
-
-    Raises:
-        TypeError: if first_name or last_name are not string
-
-    Return:
-        Nothing special"""
-
 def say_my_name(first_name, last_name=""):
+    """Task: write a function that print 'My name is <first name> <last name>'.
+        Prototype: def say_name(first_name, last_name=""):
+
+        Args:
+            first_name (str): string
+            last_name (str): string. Default value is "" (empty string)
+
+        Raises:
+            TypeError: if first_name or last_name are not string
+
+        Return:
+            Nothing special"""
     if type(first_name) != str:
         raise TypeError('first_name must be a string')
     if type(last_name) != str:
