2d8df4fb39bfb43cdcb78d8f62e0919e48444e0f
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-27 17:45:20 +0000

    Printing Firstname and Lastname | Docstring

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -17,7 +17,7 @@ def say_my_name(first_name, last_name=""):
             Nothing special"""
     if type(first_name) != str:
         raise TypeError('first_name must be a string')
-    if type(last_name) != str:
+    elif type(last_name) != str:
         raise TypeError("last_name must be a string")
 
     print(f'My name is {first_name} {last_name}')
