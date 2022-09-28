815b14b768339d66974b0c0ecdcc30e9a6793525
BLACKMouha <mohamedsenghor05gmail.com>
2022-09-28 10:30:01 +0000

    Text Indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -24,7 +24,7 @@ def text_indentation(text):
         if r_text[i] in ['.', '?', ':']:
             print(r_text[i], end='\n\n')
             if r_text[i-1] == ' ':
-                pass
+                i -= 1
         else:
             print(r_text[i], end='')
         i -= 1
