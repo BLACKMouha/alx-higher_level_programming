9a052b6f4e1bb2e356dbbd166365c7bfd7d053ba
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-22 13:25:22 +0000

    Square class definition

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,12 +10,6 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initializes a Square instance after validating the specified size"""
-        Rectangle.integer_validator(self, "size", size)
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """Computes the area of square"""
-        return self.__size ** 2
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
