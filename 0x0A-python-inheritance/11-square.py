659ad7bed26625d204605e8e780fde57b90f5859
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-22 13:37:19 +0000

    Square class definition | Implementation of __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""10-square module"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class definition based on Rectangle"""
+
+    def __init__(self, size):
+        """Initializes a Square instance after validating the specified size"""
+        Rectangle.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Computes the area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        s = "[" + str(self.__class__.__name__) + "] "
+        s += str(self.__width) + "/" + str(self.__height)
+        return s
