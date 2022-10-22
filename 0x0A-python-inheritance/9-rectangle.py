0be25b2d5acb4b520e1b83e922ca93439c0f2f01
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-22 12:24:35 +0000

    Rectangle class definition based on BaseGeometry | Overwritting area function class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""9-rectangle module"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    """Definition of Rectangle class based on BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """Initializes a Rectangle instance
+            Parameters:
+                width: integer, width
+                height: integer, height"""
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Computes the area of a rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
