d24deb9c237082efe243c2bf4d3369607e2a564c
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-22 12:10:36 +0000

    Rectangle class definition based on BaseGeometry class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""8-rectangle module"""
 
+"""8-rectangle module"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+
     """Definition of Rectangle class based on BaseGeometry class"""
 
     def __init__(self, width, height):
