f6d5d4d04f66d138163f80526354044aff894fcc
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-25 16:44:06 +0000

    Replaces all atrributes of the student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""11-student module"""
+
+
+class Student:
+    """Implementation of Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary description of an instance"""
+        new_dict = dict()
+
+        if attrs is not None:
+            for attr, value in self.__dict__.items():
+                if attr in attrs:
+                    new_dict[attr] = value
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of an instance"""
+        for attr, value in json.items():
+            setattr(self, attr, value)
