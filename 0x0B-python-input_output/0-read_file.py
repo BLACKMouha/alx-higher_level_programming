9e404a27e4de9bfd8a2dcaac8e9153379842e9d4
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-25 09:13:16 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""0-read_file module"""
+
+
+def read_file(filename=""):
+    """Opens and reads a file"""
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end='')
