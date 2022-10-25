c59cbe89b89460e25f3d2c515f6c4cf64766e17c
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-25 09:44:56 +0000

    JSON Representation of a Python object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import json
+
+"""3-to_json_string module"""
+
+
+def to_json_string(my_obj):
+    """Eeturns the JSON representation of an object"""
+    return json.dumps(my_obj)
