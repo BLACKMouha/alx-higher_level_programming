89429566ed70855964e889cf9e112333488171d8
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-25 09:29:41 +0000

    Writes a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,7 @@
 
 
 def write_file(filename="", text=""):
-    """Write `text` in `filename`"""
+    """Write `text` in `filename`
+        Return: the number of characters written"""
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
