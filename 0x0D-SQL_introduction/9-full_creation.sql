121555c478f349682baa52784dfd8d2e236c39df
BLACKMouha <mohamedsenghor05@gmail.com>
2022-11-17 12:51:46 +0000

    Table Creation and Data Insertion

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- Table Creation and Data Insertions
+
+-- SQL query that create a new table
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+-- SQL query that adds data in a table
+INSERT INTO second_table(id, name, score) VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8)
+;
