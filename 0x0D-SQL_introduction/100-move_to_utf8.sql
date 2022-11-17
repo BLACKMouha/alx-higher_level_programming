85da78e997d670166a062a2de63ab710129d73e1
BLACKMouha <mohamedsenghor05@gmail.com>
2022-11-17 15:53:47 +0000

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,13 @@
 -- 100-move_to_utf8.sql
--- SQL query that converts a database to UTF-8
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;
+-- SQL query that converts a database to UTF-8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 
 -- SQL query that converts a table to UTF-8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- SQL query that converts a column's table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER TABLE first_table MODIFY
-	`name` VARCHAR(256)
+	name VARCHAR(256)
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
