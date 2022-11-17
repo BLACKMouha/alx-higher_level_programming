9b4493c6b84388279b14a2e1f2d49e5f95afeeb6
BLACKMouha <mohamedsenghor05@gmail.com>
2022-11-17 13:13:30 +0000

    Conditional data displaying

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List all data in table based on a condition
+-- SQL query that shows ordered data of specified columns of a table if score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
