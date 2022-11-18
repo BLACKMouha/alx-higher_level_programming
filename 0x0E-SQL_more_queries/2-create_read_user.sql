abb63eb1eb8e3a0e7801732d11a921cbd80d78e9
BLACKMouha <mohamedsenghor05@gmail.com>
2022-11-18 16:36:16 +0000

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- SQL query that creates the MySQL user hbtn_0d_2
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- SQL query that grants privileges to user_0d_2 on the database hbtn_0d_2
-GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO user_0d_2@localhost;
+-- SQL query that grants SELECT privilege to user_0d_2 on the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
