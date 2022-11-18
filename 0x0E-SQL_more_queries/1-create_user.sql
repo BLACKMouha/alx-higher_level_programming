f1735c553f046bab342671015bd4838b71e8bfd2
BLACKMouha <mohamedsenghor05@gmail.com>
2022-11-18 15:54:53 +0000

    Create a user if he does not already exist

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,5 @@
 -- SQL query that creates the MySQL user 'user_0d_1'.
 CREATE USER IF NOT EXISTS
 	'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT PRIVILEGE ON *.* TO 'user_Od_1'@'localhost';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_Od_1'@'localhost';
