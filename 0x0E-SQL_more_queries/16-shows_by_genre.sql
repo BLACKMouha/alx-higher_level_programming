f35a875b30c4023ab837819fb91a94abe2511c6f
BLACKMouha <mohamedsenghor05@gmail.com>
2022-11-19 20:13:46 +0000

    List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@
 -- from the database hbtn_0d_tvshows.
 SELECT title, name
 	FROM tv_shows
-	INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-	INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 	ORDER BY title, name;
