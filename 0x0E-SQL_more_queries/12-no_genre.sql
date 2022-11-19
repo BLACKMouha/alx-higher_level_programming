9a5894890e57c6c139705e2320049c2c0ce0a88b
BLACKMouha <mohamedsenghor05@gmail.com>
2022-11-19 11:59:07 +0000

    No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,8 @@
+-- cat 12-no_genre.sql
+-- SQL query that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
 	FROM tv_shows
 	LEFT OUTER JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
-	WHERE tv_shows.id NOT IN (SELECT DISTINCT tv_show_genres.show_id FROM tv_show_genres)
+	WHERE tv_show_genres.genre_id IS  NULL
 	ORDER BY tv_shows.title, tv_show_genres.genre_id;
