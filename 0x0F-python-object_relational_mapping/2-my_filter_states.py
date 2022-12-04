630bf5ef522ea5a3eb1c5ddb349c23d2880cead9
BLACKMouha <mohamedsenghor05@gmail.com>
2022-12-04 21:26:50 +0000

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""2-my_filter_states.py"""
+
+
+if __name__ == '__main__':
+    """import modules"""
+    import MySQLdb
+    from sys import argv
+
+    if len(argv) >= 5:
+        """Retrieve passed arguments"""
+        username = argv[1]
+        password = argv[2]
+        dbname = argv[3]
+        search = argv[4]
+
+        """Establish connection to the database"""
+        conn = MySQLdb.connect(
+                host='localhost',
+                user=username,
+                passwd=password,
+                db=dbname,
+                port=3306,
+                charset='utf8')
+
+        """Create the cursor instance"""
+        cur = conn.cursor()
+        """SQL query that list all states beginning with 'N'"""
+        cur.execute("SELECT * FROM states WHERE name='{}'".format(search))
+
+        rows = cur.fetchall()
+        """Printing row"""
+        for row in rows:
+            print(row)
+
+        """Over the game!"""
+        cur.close()
+        conn.close()
