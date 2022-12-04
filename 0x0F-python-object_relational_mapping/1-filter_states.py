7ec714f966e5aa75d85d57e772e6c16dd0e333ee
BLACKMouha <mohamedsenghor05@gmail.com>
2022-12-04 20:21:22 +0000

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,34 +2,37 @@
 
 """1-filter_states.py"""
 
-"""import modules"""
-import MySQLdb
-from sys import argv
+if __name__ == '__main__':
+    """import modules"""
+    import MySQLdb
+    from sys import argv
 
-"""Retrieve passed arguments"""
-username = argv[1]
-password = argv[2]
-dbname = argv[3]
 
-"""Establish connection to the database"""
-conn = MySQLdb.connect(
-        host='localhost',
-        user=username,
-        passwd=password,
-        db=dbname,
-        port=3306,
-        charset='utf8')
+    if len(argv) == 4:
+        """Retrieve passed arguments"""
+        username = argv[1]
+        password = argv[2]
+        dbname = argv[3]
 
-"""Create the cursor instance"""
-cur = conn.cursor()
-"""SQL query that list all states beginning with 'N'"""
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        """Establish connection to the database"""
+        conn = MySQLdb.connect(
+                host='localhost',
+                user=username,
+                passwd=password,
+                db=dbname,
+                port=3306,
+                charset='utf8')
 
-rows = cur.fetchall()
-"""Printing rows"""
-for row in rows:
-    print(row)
+        """Create the cursor instance"""
+        cur = conn.cursor()
+        """SQL query that list all states beginning with 'N'"""
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
 
-"""Over the game!"""
-cur.close()
-conn.close()
+        rows = cur.fetchall()
+        """Printing rows"""
+        for row in rows:
+            print(row)
+
+        """Over the game!"""
+        cur.close()
+        conn.close()
