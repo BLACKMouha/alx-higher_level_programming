a136e8af47e28061d72e6a61d7b07b5521ed2a23
BLACKMouha <mohamedsenghor05@gmail.com>
2022-12-04 18:47:32 +0000

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+username = argv[1]
+password = argv[2]
+dbname = argv[3]
+
+conn = MySQLdb.connect(
+    host='localhost',
+    user=username,
+    passwd=password,
+    db=dbname,
+    port=3306,
+    charset='utf8')
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+rows = cur.fetchall()
+
+for row in rows:
+    print("{}".format(row))
