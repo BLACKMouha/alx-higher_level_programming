226f4241d6aad16b8020da38dec8ab6f5346aed4
BLACKMouha <mohamedsenghor05@gmail.com>
2022-12-04 19:39:55 +0000

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
+"""1-filter_states.py"""
+
+"""import modules"""
 import MySQLdb
 from sys import argv
 
+"""Retrieve passed arguments"""
 username = argv[1]
 password = argv[2]
 dbname = argv[3]
+
+"""Establish connection to the database"""
 conn = MySQLdb.connect(
         host='localhost',
         user=username,
@@ -13,13 +19,17 @@ conn = MySQLdb.connect(
         db=dbname,
         port=3306,
         charset='utf8')
+
+"""Create the cursor instance"""
 cur = conn.cursor()
+"""SQL query that list all states beginning with 'N'"""
 cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
 
 rows = cur.fetchall()
-
+"""Printing rows"""
 for row in rows:
     print(row)
 
+"""Over the game!"""
 cur.close()
 conn.close()
