553009844a6040c60119d361c36bfd07eed16818
BLACKMouha <mohamedsenghor05@gmail.com>
2023-01-16 20:40:17 +0000

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """hbnb_status module"""
-import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    body = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(body)))
-    print('\t- content: {}'.format(body))
-    print('\t- utf-8 content: {}'.format(body.decode('utf-8')))
+if __name__ == '__main__':
+    import urllib.request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
