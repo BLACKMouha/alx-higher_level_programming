32324daa7daa214c1f016447cb4cc280c930df14
BLACKMouha <mohamedsenghor05@gmail.com>
2023-01-18 10:38:27 +0000

    Retrieving 10 commits of a Github repository

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""100-github_commits module
+"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from requests import get
+
+    owner = argv[1]
+    repo = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    res = get(url, headers={"Accept": "application/vnd.github+json"})
+    commits = res.json()
+    for i in range(10):
+        print('{}: {}'.format(
+            commits[i]['sha'], commits[i]['commit']['author']['name']))
