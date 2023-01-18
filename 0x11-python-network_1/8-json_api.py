69f60d3c0d578bbdc8581e6949ab3d66127f8649
BLACKMouha <mohamedsenghor05@gmail.com>
2023-01-18 08:16:53 +0000

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,14 +9,14 @@ if __name__ == '__main__':
     import requests
     from sys import argv
 
-    letter = "" if not argv[1] or len(argv[1]) != 1 else argv[1]
-
+    letter = "" if len(argv) != 2 or len(argv[1]) != 1 else argv[1]
     url = 'http://0.0.0.0:5000/search_user'
-    res = requests.post(url, data={'q': letter})
-    json = res.json()
-    if not (isinstance(json, dict)):
+    try:
+        res = requests.post(url, data={'q': letter})
+        json = res.json()
+        if not json:
+            print('No result')
+        if 'id' in json and 'name' in json:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
+    except Exception:
         print('Not a valid JSON')
-    if not json:
-        print('No result')
-    if 'id' in json and 'name' in json:
-        print("[{}] {}".format(json.get('id'), json.get('name')))
