4bfcb9fc1496e9ad8e705ef6b905198e04b59048
BLACKMouha <mohamedsenghor05@gmail.com>
2023-02-07 13:41:59 +0000

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
-const n = Number(process.argv[2]);
-let r = 1;
-if (isNaN(n) || n === 0 || n === 1) {
-  console.log(r);
-} else {
-  for (let i = 2; i <= n; i++) {
-    r *= i;
+function factorial (a) {
+  const n = Number(a);
+  let r = 1;
+
+  if (isNaN(n) || n === 0 || n === 1) {
+    console.log(r);
   }
-  console.log(r);
+  else {
+    for (let i = 2; i <= n; i++) {
+      r *= i;
+    }
+    console.log(r);
+  }
 }
+
+factorial(process.argv[2]);
