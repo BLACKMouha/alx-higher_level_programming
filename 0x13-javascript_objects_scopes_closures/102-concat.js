303ebc779fe8bb1decfb7e74cb2fbe386629be6a
BLACKMouha <mohamedsenghor05@gmail.com>
2023-02-09 18:17:17 +0000

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,16 +4,16 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-fs.readFile('fileA', function (errA, dataA) {
+fs.readFile(fileA, function (errA, dataA) {
   if (errA) throw errA;
 
-  fs.readFile('fileB', function (errB, dataB) {
+  fs.readFile(fileB, function (errB, dataB) {
     if (errB) throw errB;
 
     dataA = dataA.toString();
     dataB = dataB.toString();
 
-    fs.writeFile('fileC', dataA.concat(dataB), function (errC) {
+    fs.writeFile(fileC, dataA.concat(dataB), function (errC) {
       if (errC) throw errC;
     });
   });
