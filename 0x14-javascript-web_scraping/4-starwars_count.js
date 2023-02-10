f91b38fa1c7d8c868ef3a2bcc40feb89c32f2bff
BLACKMouha <mohamedsenghor05@gmail.com>
2023-02-10 19:55:10 +0000

    Tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 
-const request = require('request');
+if (process.argv.length === 3) {
+  const request = require('request');
+  const url = process.argv[2];
 
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-request(url, function (error, response, body) {
-  if (error) console.log('error:', error);
-  if (response.statusCode === 404) {
-    console.log('statusCode:', response.statusCode);
-  }
-  const results = JSON.parse(body).results;
-  let c = 0;
-  for (const result of results) {
-    const characters = result.characters;
-    for (const character of characters) {
-      if (character === 'https://swapi-api.alx-tools.com/api/people/18/') c += 1;
+  request(url, function (error, response, body) {
+    if (error) console.log('error:', error);
+    if (response.statusCode === 404) {
+      console.log('statusCode:', response.statusCode);
     }
-  }
-  console.log(c);
-});
+    const results = JSON.parse(body).results;
+    let c = 0;
+    for (const result of results) {
+      const characters = result.characters;
+      for (const character of characters) {
+        if (character === 'https://swapi-api.alx-tools.com/api/people/18/') c += 1;
+      }
+    }
+    console.log(c);
+  });
+}
