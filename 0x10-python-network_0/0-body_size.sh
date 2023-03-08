12dfc400660ae3d7116c055c8be580187deedad7
BLACKMouha <96888801+BLACKMouha@users.noreply.github.com>
2023-03-08 15:08:33 +0000

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Takes in a URL and displays the size of the HTTP response body
-curl -sI "$1" | grep 'Content-Length' | cut -d ' ' -f 2
--- 0x10-python-network_0/4-header.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Takes an URL and displays all HTTP methods the server will accept
-curl -sH "X-School-User-Id: 98" "$1"
