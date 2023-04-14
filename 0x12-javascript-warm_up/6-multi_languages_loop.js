2a785cbcf2f9afa8ad9c19d88b78d30c1dfe0846
BLACKMouha <mohamedsenghor05gmail.com>
2023-04-14 15:43:29 +0000

    JavaScript not Javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 arr.forEach((val) => { console.log(val); });
