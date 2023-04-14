b2667cba5a7d604035cdd85d6396b76528f08587
BLACKMouha <mohamedsenghor05gmail.com>
2023-04-14 15:44:54 +0000

    change '#' to 'X'

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const n = Math.floor(Number(process.argv[2]));
 if (isNaN(n)) {
   console.log('Missing size');
 } else {
-  const s = '#'.repeat(n);
+  const s = 'X'.repeat(n);
   for (let i = 0; i < n; i++) {
     console.log(s);
   }
