74008f053b6cd400f10a63ab6be38591df8a5eb5
BLACKMouha <mohamedsenghor05gmail.com>
2023-04-15 15:43:36 +0000

    SQLAlchemy | Map state SQL table to State Python class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+model_state module
+'''
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
@@ -7,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    '''
+    Representation of a state
+    '''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
