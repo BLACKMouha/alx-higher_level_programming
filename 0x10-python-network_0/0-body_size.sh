d41e2f8e3439f02808df200c9ab9734baf396001
BLACKMouha <mohamedsenghor05gmail.com>
2023-04-28 19:57:30 +0000

    Displays the value of Content-Lenght HTTP responde header

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Takes in a URL, sends a request to that URL then and displays the size of the
+# body of the response
+curl -Is "$1" | grep Content-Length | cut --delimiter=' ' --fields=2
