976d499c34e08dd16af52d4a2e5f5a206f6b2967
BLACKMouha <mohamedsenghor05gmail.com>
2023-04-28 20:14:21 +0000

    Shebang fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/env bash
+#!/bin/bash
 # Takes in a URL, sends a request to that URL then and displays the size of the body of the response
 curl -Is "$1" | grep Content-Length | cut --delimiter=' ' --fields=2
