bd38939326e3078e27699965ffce207c7a834962
BLACKMouha <mohamedsenghor05gmail.com>
2023-04-29 15:57:23 +0000

    Tasks

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all methods the server will acccept
-curl -siX "$1" "OPTIONS" | grep "Allow" | cut --delimiter=' ' -f2-
+curl -siX "$1" "OPTIONS" | grep "Allow" | cut -d=' ' -f2-
