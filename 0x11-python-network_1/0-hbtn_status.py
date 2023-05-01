7fea67fa9075b7d511f9af91ab9a23c2214b2a32
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-01 12:13:53 +0000

    Fetches ALX intranet status page and displays infos about the response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if (__name__ == '__main__'):
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as res:
         body = res.read()
-        print('Body reponse:')
+        print('Body response:')
         print('\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
               .format(type(body), body, res.msg))
