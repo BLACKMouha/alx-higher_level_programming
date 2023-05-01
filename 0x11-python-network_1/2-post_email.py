69d29dde611c625dc0de47b2baf4a65657048ccd
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-01 13:48:50 +0000

    Sends a POST request to the URL with the email, both passed as arguments

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,9 +8,9 @@ from sys import argv
 
 if (__name__ == '__main__'):
     url = argv[1]
-    email = argv[2]
-    value = {'email': email}
-    data = urllib.parse.urlencode(value)
+    data = {}
+    data['email'] = argv[2]
+    data = urllib.parse.urlencode(data)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as res:
