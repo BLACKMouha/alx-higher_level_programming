4f4b32dbe7e7c3b46114502a744f26e0cec07c5f
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-03 21:23:59 +0000

    Make files excecutable

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,10 +7,10 @@ import urllib.request
 from sys import argv
 
 if (__name__ == '__main__'):
-    url = argv[1]
+    req = argv[1]
     data = {'email': argv[2]}
     data = urllib.parse.urlencode(data)
     data = data.encode('ascii')
-    with urllib.request.urlopen(url, data) as res:
+    with urllib.request.urlopen(req, data) as res:
         body = res.read()
         print(body.decode('utf-8'))
