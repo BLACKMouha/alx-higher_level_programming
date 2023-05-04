d9a1d1c7f6104b8ade2f2c2b8462e967aff914b7
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-04 16:24:25 +0000

    pycodestyle compliance

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ import requests
 if (__name__ == '__main__'):
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(r.text),
-                                                               r.text))
+                                                                 r.text))
