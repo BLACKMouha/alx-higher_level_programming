792e2f27af1b5ec73c6dadf1d6195136efc7e09c
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-04 21:01:33 +0000

    Prevent SQL Injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,13 +7,12 @@ if (__name__ == '__main__'):
     import MySQLdb
     from sys import argv
 
-    if (len(argv) == 6):
+    if (len(argv) == 5):
         h, u, p, d = 'localhost', argv[1], argv[2], argv[3]
         db = MySQLdb.connect(host=h, user=u, passwd=p, db=d, port=3306)
         cur = db.cursor()
         searched_state = argv[4]
-        q = 'SELECT * FROM states WHERE name = "{}"'.format(searched_state)
-        cur.execute(q)
+        cur.execute('SELECT * FROM states WHERE name=%s', (searched_state,))
         r = cur.fetchall()
         for i in r:
             print(i)
