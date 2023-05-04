b74101ef9510b299c8ce07939dbc537d9c9e90ef
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-04 16:22:01 +0000

    using requests module to fetch ALX intranet

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+Fetches ALX intranet status page and displays some info
+'''
+import requests
+
+
+if (__name__ == '__main__'):
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:\n\t-type: {}\n\t-content: {}'.format(type(r.text),
+                                                               r.text))
