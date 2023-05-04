f38e7fbf7765eb49c1bf6980cb20c5ba693f714f
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-04 16:32:57 +0000

    Displays the X-Request-Id header of a URL passed as argument

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Fetches a URL and displays X-Request-Id header
+'''
+import requests
+from sys import argv
+
+if (__name__ == '__main__'):
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])
