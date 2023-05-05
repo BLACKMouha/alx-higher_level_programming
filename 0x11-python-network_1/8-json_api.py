c9ad9dddf33a71bd893bf931e98a3d2bea50a7e3
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-05 07:29:43 +0000

    Sends a POST requests a uses the JSON format of the response to display information if any

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,21 +4,19 @@ Sends a POST requests with a letter as data to a URL both passed as arguments
 then displays id and name from the JSON if any
 '''
 
-from requests import post
-from requests.exceptions import JSONDecodeError
 from sys import argv
-
+import requests
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
     q = argv[1] if len(argv) == 2 else ""
     try:
-        r = post(url, data={"q": q})
+        r = requests.post(url, data={"q": q})
         r.raise_for_status()
         rj = r.json()
         if (not rj):
             print('No result')
         else:
             print('[{}] {}'.format(rj.get('id', None), rj.get('name', None)))
-    except JSONDecodeError:
+    except requests.execptions.JSONDecodeError:
         print('Not a valid JSON')
