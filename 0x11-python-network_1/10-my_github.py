79a7d6bc7c530ff01cb9ee520cea5765c57266be
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-05 11:20:16 +0000

    Takes GitHub credentials (username and password) and uses the GitHub API to display the id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,16 +4,16 @@ Takes GitHub credentials and displays the id
 '''
 
 if __name__ == '__main__':
-    from requests import get, post
-    from requests.auth import HTTPBasicAuth
     from sys import argv
+    from requests import get
+    from requests.auth import HTTPBasicAuth
 
     user, token = argv[1], argv[2]
     basic = HTTPBasicAuth(user, token)
     url = 'https://api.github.com/user'
     headers = {}
     headers['X-GitHub-Api-Version'] = '2022-11-28'
-    r = post(url, headers=headers, auth=basic)
+    r = get(url, headers=headers, auth=basic)
     j = eval(r.text.replace('false', 'False').replace('true', 'True')
                    .replace('null', 'None'))
     print(j.get('id', None))
