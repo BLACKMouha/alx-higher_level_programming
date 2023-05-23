5bcd0406ccaa831d78e31fb19284bd22ce6c7f39
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-23 17:57:45 +0000

    Tries to write a text to a file and print the written content or error message if any

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * Writes a string to a file
+ * the first argument is the file path
+ * the second argument is the string to write
+ * the content of the file must be written in utf-8
+ * if an erro occured during while writing, print the error object
+ */
+
+if (process.argv.length === 4) {
+  const fs = require('fs');
+  const f = process.argv[2];
+  const c = process.argv[3];
+  fs.writeFile(f, c, 'utf-8', function (e) {
+    if (e) {
+      console.error(e);
+    } else {
+      console.log(c);
+    }
+  });
+}
