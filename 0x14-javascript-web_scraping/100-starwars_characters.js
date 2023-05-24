83454d7c539e91caf6d3434509c467cb9263ecb3
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-24 06:34:44 +0000

    Prints all characters of a Star Wars Movie referenced by its ID

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/**
+ * Prints all characters of a Star Wars movie, one by line
+ * The first argument is an integer, the Movie ID
+ * Used the Star Wars API and the module 'request'
+ */
+
+if (process.argv.length === 3) {
+  const request = require('request');
+  const id = process.argv[2];
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+  request.get(url, function (e, r, b) {
+    if (e) {
+      throw (e);
+    } else {
+      for (const p of JSON.parse(b).characters) {
+        request.get(p, function (x, s, m) {
+          if (x) {
+            throw (e);
+          } else {
+            console.log(JSON.parse(m).name);
+          }
+        });
+      }
+    }
+  });
+}
